74d5a551941aff0fb7aed5ca1020e8f3d1cadef8
Hrushikesh Paraye <parayehrushikesh10@gmail.com>
2026-09-07 22:02:18 +0530

    Cart Add!

--- src/components/menu/FoodCustomizationModal.jsx
+++ src/components/menu/FoodCustomizationModal.jsx
@@ -59,7 +59,7 @@ const FoodCustomizationModal = ({
     };
 
     return (
-        <div className="fixed inset-0 z-[60] flex items-center justify-center p-4">
+        <div className="fixed inset-0 z-60 flex items-center justify-center p-4">
             <div
                 className="absolute inset-0 bg-black/50"
                 onClick={onClose}
